405a2703d29ff8e3c491798b0ac28383878e48b8
qadeer <qadeerabdul182006@gmail.com>
2025-02-11 21:10:07 +0530

    for loop

---
+++ for loop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=10;i>=1;i--)
+        {
+            System.out.print(n*i+" ");
+        }
+    
+        
+    }
+}
